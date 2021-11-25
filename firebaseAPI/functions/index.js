997744356419f85e663e1c6e4e49137c0addc6bc
jhuh3226 <jhuh3226@gmail.com>
2021-11-25 14:40:17 -0500

    Update index.js

--- firebaseAPI/functions/index.js
+++ firebaseAPI/functions/index.js
@@ -10,6 +10,100 @@
 // subscribe to MQTT channels
 // when data arrives, send to FireBase
 
+// include the MQTT library:
+const mqtt = require("mqtt");
+
+// MQTT broker details:
+let broker = {
+    hostname: 'public.cloud.shiftr.io',
+    port: 443
+};
+
+// MQTT client:
+let client;
+// client credentials:
+let creds = {
+    clientID: 'htmlClient',
+    userName: 'public',
+    password: 'public'
+}
+// topic to subscribe to when you connect to the broker:
+let topic = 'jReadings';
+let localDiv, remoteDiv;
+
+let msg = null;
+
+function setup() {
+    // Create an MQTT client:
+    client = new Paho.MQTT.Client(broker.hostname, Number(broker.port), creds.clientID);
+    // set callback handlers for the client:
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+    // connect to the MQTT broker:
+    client.connect(
+        {
+            onSuccess: onConnect,       // callback function for when you connect
+            userName: creds.userName,   // username
+            password: creds.password,   // password
+            useSSL: true                // use SSL
+        }
+    );
+    // add handles for the divs:
+    localDiv = document.getElementById('local');
+    remoteDiv = document.getElementById('remote');
+}
+
+
+// called when the client connects
+function onConnect() {
+    localDiv.innerHTML = 'client is connected to '
+        + broker.hostname + '<br>and subscribed to '
+        + topic;
+    client.subscribe(topic);
+}
+
+// called when the client loses its connection
+function onConnectionLost(response) {
+    if (response.errorCode !== 0) {
+        localDiv.innerHTML = 'Connection Lost:' + response.errorMessage;
+    }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+    // get the payload string and make it a JSON object:
+    msg = JSON.parse(message.payloadString);
+    // console.log(message.payloadString);
+    // how to read each value, this case it's lux
+    console.log(msg);
+
+    /* firebase */
+    // sending data to firebase
+    // addDocuments(msg);
+    // getting data from firebase
+    // getDocuments();
+
+    // iterate over the elements of the JSON object:
+    for (var key in msg) {
+        // If there is not an HTML element with the same ID:
+        if (document.getElementById(key) === null) {
+            // create create one and give it this ID:
+            let thisDiv = document.createElement('div');
+            thisDiv.id = key;
+            // create text with the key and value:
+            let textNode = document.createTextNode(key + ': ' + msg[key]);
+            // add the text to the element and add it to the HTML:
+            thisDiv.append(textNode);
+            document.body.append(thisDiv);
+            // console.log(textNode);
+        } else {
+            // if there's already an element with this ID,
+            // just update it:
+            let thisDiv = document.getElementById(key);
+            thisDiv.innerHTML = key + ': ' + msg[key];
+        }
+    }
+}
 
 // firebase client = express server
 // when web client requests data,
@@ -80,12 +174,12 @@ app.get('/api/read/:id', (req, res) => {
 
 });
 
-function getLatest (req, res) {
+function getLatest(req, res) {
     // get what the client wants from the request
     // form it into a fireBase query
     // query fireBase
     // when the response comes in, respond to the web client
-  }
+}
 
 
 // export the api to firebase cloud functions
